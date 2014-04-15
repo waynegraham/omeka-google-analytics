8c4cac8cb7c691f1e51cf97be193ebeb04f6fd86
Wayne Graham <wayne.graham@virginia.edu>
2014-04-15 09:47:22 -0400

    Updates to use Google Universe page tracking

--- GoogleAnalyticsPlugin.php
+++ GoogleAnalyticsPlugin.php
@@ -30,7 +30,14 @@ class GoogleAnalyticsPlugin extends Omeka_Plugin_AbstractPlugin
     {
         $gaKey = get_option('google_analytics_key'); // google analytics key
         $analytics =<<< ANALYTICS
-<script>var _gaq=[['_setAccount','$gaKey'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=true;g.src=('https:'==location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s);})(document,'script');</script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '$gaKey', 'auto');
+ga('send', 'pageview');
+</script>
 ANALYTICS;
 
         queue_js_string($analytics);
